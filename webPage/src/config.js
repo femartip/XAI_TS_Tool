31819f1d631461924717b25ab1bcb2aa235012c2
femartip <felix.marti99@gmail.com>
2025-09-15 10:44:17 +0200

    ws_base added to .config

--- webPage/src/config.js
+++ webPage/src/config.js
@@ -1,4 +1,4 @@
 export const API_HTTP_BASE = 'http://158.42.185.235:1337/api';
 export const apiUrl = (p) => `${API_HTTP_BASE}${p.startsWith('/') ? '' : '/'}${p}`;
-
+export const WS_BASE = 'ws://158.42.185.235:1337/api';
 
